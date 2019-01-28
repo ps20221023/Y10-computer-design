30c514bdde450392851d4e591b22046a9b2e6b2a
ps20221023 <46880403+ps20221023@users.noreply.github.com>
2019-01-28 12:09:41 +0800

    Add files via upload

---
+++ Tutorial 1.1/sketch.js
@@ -0,0 +1,56 @@
+function setup()
+{
+  //create a drawing service 700px wide, 500px tall
+  createCanvas(700,500);
+
+}
+
+function draw()
+{
+  //create a background using RGB values
+  background(112,219,100)
+
+  // change the paint brush to a specific color according to RGB values
+  fill	(126,175,218)  ;
+
+  // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
+  rect(10,10,10,50);
+  rect(10,10,50,10);
+  rect(10,50,50,10);
+  rect(50,10,10,50);
+  rect(70,10,10,50);
+  rect(100,10,10,50);
+  rect(100,25,12,10);
+  rect(100,30,12,10);
+  rect(103,25,12,10);
+  rect(110,20,12,10);
+  rect(120,15,12,10);
+  rect(103,30,12,10);
+  rect(110,40,12,10);
+  rect(120,50,12,10);
+  rect(160,10,10,50);
+  rect(165,15,10,10);
+  rect(170,20,10,10);
+  rect(175,25,10,10);
+  rect(180,30,10,10);
+  rect(185,35,10,10);
+  rect(190,40,10,10);
+  rect(195,45,10,10);
+  rect(200,40,10,10);
+  rect(205,35,10,10);
+  rect(210,30,10,10);
+  rect(215,25,10,10);
+  rect(220,20,10,10);
+  rect(225,15,10,10);
+  rect(230,10,10,50);
+  rect(250,30,10,10);
+  rect(255,20,10,10);
+  rect(255,40,10,10);
+  rect(260,15,10,10);
+  rect(265,10,10,10);
+  rect(260,45,10,10);
+  rect(265,50,10,10);
+  rect(270,9,10,10);
+  rect(270,49,10,10);
+  rect()
+}
