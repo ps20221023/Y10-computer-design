2ad1fe4d167f81c89ccaa10accbfd943c0922763
ps20221023 <46880403+ps20221023@users.noreply.github.com>
2019-01-28 12:50:04 +0800

    Add files via upload

--- Tutorial 1.1/sketch.js
+++ Tutorial 1.1/sketch.js
@@ -14,11 +14,14 @@ function draw()
   fill	(126,175,218)  ;
 
   // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
+  // a letter
   rect(10,10,10,50);
   rect(10,10,50,10);
   rect(10,50,50,10);
   rect(50,10,10,50);
   rect(70,10,10,50);
+
+  // another letter
   rect(100,10,10,50);
   rect(100,25,12,10);
   rect(100,30,12,10);
@@ -53,6 +56,8 @@ function draw()
   rect(270,9,10,10);
   rect(270,49,10,10);
   rect(10,100,50,50);
+
+
 rect(10,90,90,90)
 rect(200,200,200,200);
 rect(501,312,34,125);
@@ -60,7 +65,8 @@ rect(500,400,300,400)
 rect(1,1,202,100)
 rect(1,400,200,150)
 rect(500,1,300,200)
-
+rect(300,200,100,100)
+rect(700/2-100/2,500/2-100/2,100,100)
 
 
 
