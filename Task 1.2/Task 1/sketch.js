bbe3085c6527e7955902377c691830ff8539864b
ps20221023 <46880403+ps20221023@users.noreply.github.com>
2019-01-30 11:36:09 +0800

    Add files via upload

---
+++ Task 1.2/Task 1/sketch.js
@@ -0,0 +1,34 @@
+function setup()
+{
+  createCanvas(700,500);
+
+}
+
+function draw()
+{
+	background(123,231,412)
+
+	// change the paint brush to a specific color according to RGB values
+	// this color applies only to the inside color of a shape
+	fill(123,3,255);
+
+	// change the paint brush to a specific color according to RGB values
+	// this color applies only to the outside color of a shape
+	stroke(251,2,351);
+
+	// modifies the thickness of the border of an object
+	strokeWeight(20);
+
+
+	rect(10,10,100,50);
+	rect(10,100,100,50);
+
+
+	fill(521,235,261);
+	stroke(215,255,25);
+	strokeWeight(20);
+
+	rect(150,10,100,50);
+	rect(150,100,100,50);
+
+}
---
+++ Task 1.2/Task 3/sketch.js
@@ -0,0 +1,45 @@
+function setup()
+{
+  createCanvas(700,500);
+
+}
+
+function draw()
+{
+	background(123,231,412)
+
+	// change the paint brush to a specific color according to RGB values
+	// this color applies only to the inside color of a shape
+	fill(56,3,255);
+
+	// change the paint brush to a specific color according to RGB values
+	// this color applies only to the outside color of a shape
+	stroke(251,2,351);
+
+	// modifies the thickness of the border of an object
+	strokeWeight(10);
+
+
+	rect(10,10,100,50);
+	rect(10,100,100,50);
+
+
+	fill(521,235,261);
+	stroke(215,255,25);
+	strokeWeight(20);
+
+	rect(300,10,100,50);
+	rect(400,10,100,50);
+
+
+stroke(231,21,123);
+fill(123,321,541);
+rect(300,100,100,50);
+rect(400,100,100,50);
+
+stroke(432,123,632)
+fill(156,213,123);
+rect(400,200,100,100);
+rect(300,200,100,100);
+
+}
