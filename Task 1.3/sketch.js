9a0a34506faa16f2ac5c0eaf3fac22e9cda41426
ps20221023 <46880403+ps20221023@users.noreply.github.com>
2019-02-18 12:35:55 +0800

    Add files via upload

---
+++ Task 1.3/sketch.js
@@ -0,0 +1,35 @@
+function setup()
+{
+  createCanvas(700,500);
+
+}
+
+function draw()
+{
+	background(255,0,0)
+
+	fill(133,23,123);
+	stroke(2,123,465);
+	strokeWeight(8);
+	text("Hello World",500,30);
+
+	rect(10,10,100,50);
+textSize(16)
+  fill(12,412,532)
+	text("Yorfrie Wong",20,50);
+
+
+fill(243,123,653);
+	rect(10,100,100,50);
+
+	fill(255,255,255);
+
+	text("Donald Trump",10,130);
+
+
+fill(0,0,255);
+rect(10,100,100,50);
+
+
+
+}
